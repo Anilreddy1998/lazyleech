fcdeb7377d351b3576cf6bdea62b483ca6c871a1
Anilreddy1998 <86147406+Anilreddy1998@users.noreply.github.com>
2021-06-23 14:27:21 +0530

    Update __init__.py

--- lazyleech/__init__.py
+++ lazyleech/__init__.py
@@ -20,16 +20,15 @@ import aiohttp
 from io import BytesIO, StringIO
 from pyrogram import Client
 
-API_ID = os.environ.get('6265023')
-API_HASH = os.environ.get('48006bd1f43b2a3978c3abc1e7417be8
- ')
-BOT_TOKEN = os.environ.get('1834743468:AAEicMjhu_Uk09iftXtfccgbMB-IRjNRYh0')
-TESTMODE = os.environ.get('TESTMODE')
+API_ID = 6265023
+API_HASH = '48006bd1f43b2a3978c3abc1e7417be8'
+BOT_TOKEN = '1834743468:AAEicMjhu_Uk09iftXtfccgbMB-IRjNRYh0'
+TESTMODE = False
 TESTMODE = TESTMODE and TESTMODE != '0'
 
-EVERYONE_CHATS = os.environ.get('-1001204697354')
+EVERYONE_CHATS = '-1001204697354'
 EVERYONE_CHATS = list(map(int, EVERYONE_CHATS.split(' '))) if EVERYONE_CHATS else []
-ADMIN_CHATS = os.environ.get('-1001339692999')
+ADMIN_CHATS = '-1001339692999'
 ADMIN_CHATS = list(map(int, ADMIN_CHATS.split(' '))) if ADMIN_CHATS else []
 ALL_CHATS = EVERYONE_CHATS + ADMIN_CHATS
 
